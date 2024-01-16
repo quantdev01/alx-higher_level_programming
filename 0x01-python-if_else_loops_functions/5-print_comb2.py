e9a68aa3c89b64e26fcebab0f7e033ca6597e5d1
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-16 22:09:05 +0200

    Done with print 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if i < 10:
-        print('0{}, '.format(i), end='')
+    if i != 99:
+        print('{:02d}, '.format(i), end='')
     else:
-        if i != 99:
-            print('{}, '.format(i), end='') 
-        else:
-            print(i)
+        print('{}'.format(i)) 
