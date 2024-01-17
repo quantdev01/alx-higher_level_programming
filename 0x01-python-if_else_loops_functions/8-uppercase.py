5c27c9e7d77f3c2aac2d1d4cb3414e809da02e8e
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-17 15:16:57 +0200

    made ctrl-z

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,12 +4,12 @@ def uppercase(str):
     for i in range(len(str)):
         if (i == len(str)-1):
             x = str[i]
-            if (ord(x) >= 32 and ord(x) <= 96 or ord(x) == 0):
+            if (ord(x) >= 32 and ord(x) <= 96):
                 to_lower = ord(x) + 32
                 x = chr(to_lower)
             to_upper = ord(x) - 32
             x = chr(to_upper)
-            print('{}\n'.format(x))
+            print('{}'.format(x))
         else:
             y = str[i]
             if (ord(y) >= 32 and ord(y) <= 96):
