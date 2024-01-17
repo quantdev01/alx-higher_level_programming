667c9bd40d3a5cd80faae1c342977d0f5e7349b1
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-17 16:34:30 +0200

    Maybe fixed this

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,19 +2,11 @@
 
 def uppercase(str):
     for i in range(len(str)):
-        if (i == len(str)-1):
-            x = str[i]
-            if (ord(x) >= 0 and ord(x) <= 96):
-                to_lower = ord(x) + 32
-                x = chr(to_lower)
-            to_upper = ord(x) - 32
-            x = chr(to_upper)
-            print('{}'.format(x))
-        else:
-            y = str[i]
-            if (ord(y) >= 32 and ord(y) <= 96):
-                to_lower = ord(y) + 32
-                y = chr(to_lower)
-            to_upper = ord(y) - 32
-            y = chr(to_upper)
-            print('{}'.format(y), end='')
+        char = str[i]
+        #Check if the character is lowercase
+        if (ord(char) >= 97 and ord(char) <= 122):
+            to_upper = ord(char) - 32
+            char = chr(to_upper)
+        print('{}'.format(char), end='')
+    print() 
+
