68bcaef54cd3da3878488a7be1d062b7b1836507
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-17 15:10:28 +0200

    nothing to commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     for i in range(len(str)):
         if (i == len(str)-1):
             x = str[i]
-            if (ord(x) >= 32 and ord(x) <= 96):
+            if (ord(x) >= 32 and ord(x) <= 96 or ord(x) == 0):
                 to_lower = ord(x) + 32
                 x = chr(to_lower)
             to_upper = ord(x) - 32
