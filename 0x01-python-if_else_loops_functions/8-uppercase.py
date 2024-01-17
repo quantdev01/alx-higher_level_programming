90b8175aa4239e64b82e0308462d81355b38a568
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-17 15:04:51 +0200

    Fixed pycodestyle

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     for i in range(len(str)):
         if (i == len(str)-1):
             x = str[i]
-            if(ord(x) >= 32 and ord(x) <= 96):
+            if (ord(x) >= 32 and ord(x) <= 96):
                 to_lower = ord(x) + 32
                 x = chr(to_lower)
             to_upper = ord(x) - 32
@@ -12,7 +12,7 @@ def uppercase(str):
             print('{}'.format(x))
         else:
             y = str[i]
-            if(ord(y) >= 32 and ord(y) <= 96):
+            if (ord(y) >= 32 and ord(y) <= 96):
                 to_lower = ord(y) + 32
                 y = chr(to_lower)
             to_upper = ord(y) - 32
