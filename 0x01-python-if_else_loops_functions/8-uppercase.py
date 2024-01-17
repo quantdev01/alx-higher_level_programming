dbad2ebf965af3cc87ba9084358dd7078d19f246
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-17 15:01:31 +0200

    Made task number 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in range(len(str)):
+        if (i == len(str)-1):
+            x = str[i]
+            if(ord(x) >= 32 and ord(x) <= 96):
+                to_lower = ord(x) + 32
+                x = chr(to_lower)
+            to_upper = ord(x) - 32
+            x = chr(to_upper)
+            print('{}'.format(x))
+        else:
+            y = str[i]
+            if(ord(y) >= 32 and ord(y) <= 96):
+                to_lower = ord(y) + 32
+                y = chr(to_lower)
+            to_upper = ord(y) - 32
+            y = chr(to_upper)
+            print('{}'.format(y), end='')
