fcc97a5562fa11484ba66415e5bc2dd87d85770d
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-18 11:48:07 +0200

    Made myfirst toolbox work

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
-import calculator_1 as calc
+from calculator_1 import *
 
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    print('{} + {} = {}'.format(a, b, calc.add(a, b)), end = '\n')
-    print('{} - {} = {}'.format(a, b, calc.sub(a, b)), end = '\n')
-    print('{} * {} = {}'.format(a, b, calc.mul(a, b)), end = '\n')
-    print('{} / {} = {}'.format(a, b, calc.div(a, b)), end = '\n')
+    print('{} + {} = {}'.format(a, b, add(a, b)), end = '\n')
+    print('{} - {} = {}'.format(a, b, sub(a, b)), end = '\n')
+    print('{} * {} = {}'.format(a, b, mul(a, b)), end = '\n')
+    print('{} / {} = {}'.format(a, b, div(a, b)), end = '\n')
