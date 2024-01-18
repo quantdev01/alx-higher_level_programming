d3747d1c266308350ce1c7b7935d106e14aac0d9
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-18 11:33:13 +0200

    Changed some calculaition

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print('10 + 5 = {}'.format(calc.add(a, b)))
-    print('10 - 5 = {}'.format(calc.sub(a, b)))
-    print('10 * 5 = {}'.format(calc.mul(a, b)))
-    print('10 / 5 = {}'.format(calc.div(a, b)))
+    print('{} + {} = {}'.format(a, b, calc.add(a, b)))
+    print('{} - {} = {}'.format(a, b, calc.sub(a, b)))
+    print('{} * {} = {}'.format(a, b, calc.mul(a, b)))
+    print('{} / {} = {}'.format(a, b, calc.div(a, b)))
