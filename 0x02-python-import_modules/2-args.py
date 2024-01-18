fccfa681144098e46ee3029de1b855afb463caed
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-18 15:05:02 +0200

    Printed them with the corrensponding type specified

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
         print('0 arguments.'.format())
     else:
         if(lenstr <= 2):
-            print('{} argument:'.format(lenstr - 1))
+            print('{:d} argument:'.format(lenstr - 1))
         else:
-            print('{} arguments:'.format(lenstr - 1))
+            print('{:d} arguments:'.format(lenstr - 1))
         for i in range(1, len(sys.argv)):
-            print('{}: {}'.format(i, sys.argv[i]))
+            print('{:d}: {:s}'.format(i, sys.argv[i]))
