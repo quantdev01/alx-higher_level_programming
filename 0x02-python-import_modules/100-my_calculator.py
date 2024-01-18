aa3019e42656484152141e5b639c3015a51214e8
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-18 16:41:46 +0200

    Done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,22 +9,20 @@ if __name__ == "__main__":
     if (lenstr < 4):
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    s = 0
+    if (operator == '+'):
+        s = add(a, b)
+    elif (operator == '-'):
+        s = sub(a, b)
+    elif (operator == '*'):
+        s = mul(a, b)
+    elif (operator == '/'):
+        s = div(a, b)
     else:
-        a = int(sys.argv[1])
-        operator = sys.argv[2]
-        b = int(sys.argv[3])
-        s = 0
-        if (operator == '+'):
-            s = add(a, b)
-        elif (operator == '-'):
-            s = sub(a, b)
-        elif (operator == '*'):
-            s = mul(a, b)
-        elif (operator == '/'):
-            s = div(a, b)
-        else:
-            print('Unknown operator. Available operators: +, -, * and /')
-            exit(1)
-        if (operator == '+' or operator == '-' or operator == '*' or operator == '/'):
-            print(f'{a} {operator} {b} = {s}')
-            exit(0)
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    print(f'{a} {operator} {b} = {s}')
+    exit(0)
