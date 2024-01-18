1df7f62af89a9efe68b900649a08382b5419b0e4
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-18 14:48:34 +0200

    Made the file not to be executed when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,15 @@
 
 import sys
 
-if (len(sys.argv) == 1):
-    print('0 arguments.')
-else:
-    print(f'{len(sys.argv) -1} argument:')
-    for i in range(1, len(sys.argv)):
-        print(f'{i}: {sys.argv[i]}')
+if __name__ == ("__main__"):
+    lenstr = len(sys.argv)
+
+    if (lenstr == 1):
+        print('0 arguments.')
+    else:
+        if(lenstr <= 2):
+            print(f'{lenstr -1} argument:')
+        else:
+            print(f'{lenstr -1} arguments:')
+        for i in range(1, len(sys.argv)):
+            print(f'{i}: {sys.argv[i]}')
