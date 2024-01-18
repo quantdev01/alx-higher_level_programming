5d842c851339f0aa642e681aa981edc608ce5bd8
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-18 12:03:17 +0200

    Done with 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys
+
+if (len(sys.argv) == 1):
+    print('0 arguments.')
+else:
+    print(f'{len(sys.argv) -1} argument:')
+    for i in range(1, len(sys.argv)):
+        print(f'{i} : {sys.argv[i]}')
