395ca002df23b0517df4fc23aa86f3f3cdf21822
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-22 23:52:15 +0200

    Checking by reversing the list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,9 @@
 
 def print_reversed_list_integer(my_list=[]):
     len_list = len(my_list)
-    i = len_list - 1
+    i = len_list
+    my_list.reverse()
+    j = 0
 
-    while i >= 0:
-        print('{:d}'.format(my_list[i]))
-        i -= 1
+    for j in range(i):
+        print(my_list[j])
