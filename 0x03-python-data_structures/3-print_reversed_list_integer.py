ca22b327c5b3a6602e1b024350090648a2ed33e4
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-22 23:38:23 +0200

    done with 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    len_list = len(my_list)
+
+    while len_list -1 >= 0:
+        print(my_list[len_list - 1])
+        len_list -= 1
