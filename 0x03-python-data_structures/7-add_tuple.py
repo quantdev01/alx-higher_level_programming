f68f19f332e7e1dc1fdde013cbb6c608abf72c31
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-24 18:51:01 +0200

    Fixed pycodestyl

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,10 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     # If the tuples are smaller than 2, use 0 for missing integers
-    a = (tuple_a[0] if len(tuple_a) >= 1 else 0, tuple_a[1] if len(tuple_a) >= 2 else 0)
-    b = (tuple_b[0] if len(tuple_b) >= 1 else 0, tuple_b[1] if len(tuple_b) >= 2 else 0)
+    ln_a = len(tuple_a)
+    ln_b = len(tuple_b)
+    a = (tuple_a[0] if ln_a >= 1 else 0, tuple_a[1] if ln_a >= 2 else 0)
+    b = (tuple_b[0] if ln_b >= 1 else 0, tuple_b[1] if ln_b >= 2 else 0)
 
     # Calculate the sum of corresponding elements
     result = (a[0] + b[0], a[1] + b[1])
