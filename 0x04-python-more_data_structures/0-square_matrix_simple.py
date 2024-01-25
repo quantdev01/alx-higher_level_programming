e366eca389508e49922226714d5aec25b56f4a43
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-25 17:17:00 +0200

    Done with task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    temp = [item if not isinstance(item, list) else item[:] for item in matrix]
+
+    square = lambda n : n * n
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            temp[i][j] = square(matrix[i][j])
+
+    return temp
