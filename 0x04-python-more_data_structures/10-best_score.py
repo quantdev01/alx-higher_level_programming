8568fba35e2ab8c973040457b5734a8fbb9aec23
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-25 23:44:11 +0200

    Made the task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    i = 0
+    v = ''
+    if (a_dictionary is None):
+        return None
+
+    for key, value in a_dictionary.items():
+        if i == 0:
+            i = value
+            v = key
+
+        if (i > value):
+            continue
+        else:
+            i = value
+            v = key
+
+        i += 1
+
+    return v
