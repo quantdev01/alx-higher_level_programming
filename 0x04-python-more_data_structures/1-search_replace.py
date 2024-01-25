3f80a2cd226e1c873b3f6e22d7d1bad4e38c377f
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-25 18:22:08 +0200

    made the task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = [i for i in my_list]
+
+    for i in range(len(new_list) - 1):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list 
