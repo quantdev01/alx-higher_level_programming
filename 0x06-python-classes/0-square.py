984733c8bd830b5f3cac54d60da4634776b7a697
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-07 17:17:59 +0200

    Tryanna fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,18 +2,6 @@
 
 class Square:
     """The class does nothing just pass.
-
-    If the class has public attributes, they may be documented here
-    in an ``Attributes`` section and follow the same formatting as a
-    function's ``Args`` section. Alternatively, attributes may be documented
-    inline with the attribute's declaration (see __init__ method below).
-
-    Properties created with the ``@property`` decorator should be documented
-    in the property's getter method.
-
-    Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
-
+    class attribute are not there
     """
     pass
