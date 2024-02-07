d1d9a158b3377d5204f86051e971922bbe3eb06f
Daniel Kiyiki <danielngolu154@gmail.com>
2024-02-07 16:46:55 +0200

    Added new Folder

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,5 @@ import sys
 def safe_function(fct, *args):
     try:
         fct(args)
-    except ZeroDivisionError
-    except (NameError, TypeError, ValueError) as element:
+    except (ZeroDivisionError, NameError, TypeError, ValueError) as element:
         print("Exception: {}".format(element), file=sys.stderr)
