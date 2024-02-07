aa382867516297d15e2602f28647336af7440988
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-07 18:15:58 +0200

    Fixed pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,5 +8,5 @@ class Square:
     """
     python3 -c 'print(__import__("my_module").MyClass.__doc__)
     """
-    def  __init__(self, size):
+    def __init__(self, size):
         self.__size = size
