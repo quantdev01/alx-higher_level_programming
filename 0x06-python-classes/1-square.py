9f57e5d4416e93d6bfbc7725ed9d9ff1f9dffaf4
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-07 18:14:17 +0200

    Yes done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module to create a square with it size private
+"""
+
+
+class Square:
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)
+    """
+    def  __init__(self, size):
+        self.__size = size
