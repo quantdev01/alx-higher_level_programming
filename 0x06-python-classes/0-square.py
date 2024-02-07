22e28cad78806b2e985a3883e14da4d80252861d
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-07 17:26:27 +0200

    Maybe

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 class Square:
-    'print(__import__("my_module").MyClass.__doc__)'
+    """
+    python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
     pass
