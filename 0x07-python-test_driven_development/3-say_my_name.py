722270f0e926280155791177da511f5d3f809118
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-14 11:57:20 +0200

    pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,11 +8,14 @@ def say_my_name(first_name, last_name=""):
     """
     printing my name
     """
-    if type(first_name) != str and type(last_name) != str:
+    if not isinstance(first_name, str) and not isinstance(last_name, str):
+        # if type(first_name) != str and type(last_name) != str:
         raise TypeError("first and last name must be string")
-    if type(first_name) != str:
+    if not isinstance((first_name, str)):
+        # if type(first_name) != str:
         raise TypeError("first_name must be a string")
-    if type(last_name) != str:
+    if not isinstance(last_name, str):
+        # if type(last_name) != str:
         raise TypeError("last_name must be a string")
     if first_name is not None and last_name is not None:
         print("My name is {} {}".format(first_name, last_name))
