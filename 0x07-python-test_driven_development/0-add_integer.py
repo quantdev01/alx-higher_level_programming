36aad137fc2c22f6e0b12e63e9572fb75f8ddbe0
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-14 09:31:44 +0200

    Done with 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,21 +8,22 @@ def add_integer(a, b=98):
     """
     python3 -c 'print(__import__("my_module").my_function.__doc__)'
     """
-    if type(a) == bool and type(b) == str:
+
+    if isinstance(a, bool) and isinstance(b, str):
         raise TypeError("a and b must be integers")
-    if type(a) == str and type(b) == bool:
+    if isinstance(a, str) and isinstance(b, bool):
         raise TypeError("a and b must be integers")
-    if type(a) == bool and type(b) == bool:
+    if isinstance(a, bool) and isinstance(b, bool):
         raise TypeError("a and b must be integers")
-    if type(a) == bool:
+    if isinstance(a, bool):
         raise TypeError("a must be an integer")
-    if type(b) == bool:
+    if isinstance(b, bool):
         raise TypeError("b must be an integer")
-    if type(a) == str and type(b) == str or a is None and b is None:
+    if isinstance(a, str) and isinstance(b, str) or a is None and b is None:
         raise TypeError("a and b must be integers")
-    if type(a) == str or a is None:
+    if isinstance(a, str) or a is None:
         raise TypeError("a must be an integer")
-    if type(b) == str or b is None:
+    if isinstance(b, str) or b is None:
         raise TypeError("b must be an integer")
 
     a = int(a)
