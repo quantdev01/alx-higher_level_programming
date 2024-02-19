be379ab3374a5e41158dac76704cc790a6cc612f
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-19 22:42:42 +0200

    Still on task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-
+"""
+module is good
+"""
 def is_same_class(obj, a_class):
+    """
+    function module to return obj and it class
+    """
     return isinstance(obj, a_class)
