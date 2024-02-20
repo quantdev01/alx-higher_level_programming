428b0ddb911ea7b57155ac002c633a1c15dd8de8
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-20 04:15:36 +0200

    TrynaFixTask4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     """
     My function to return true if instance is a subclass
     """
-    return isinstance(obj, a_class) and not isinstance(type(obj), a_class) 
+    return isinstance(obj, a_class) and type(obj) is not a_class 
