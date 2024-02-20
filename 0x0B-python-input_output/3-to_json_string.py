a9ba5a5bb9650396996c6dcadb97c2edaf94c64c
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-20 22:21:17 +0200

    Readded json import

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-# import json
 """
 Module py to transform an object to Json
 """
 
 
+import json
 def to_json_string(my_obj):
     """returning my_obj to json
     Args:
@@ -13,9 +13,8 @@ def to_json_string(my_obj):
     Return:
         return - json object
     """
-    """if (my_obj is not type(set)):
+    if (my_obj is not type(set)):
         return json.dumps(my_obj)
     else:
-        return list(my_obj)
-        """
+        return 
     print('created')
