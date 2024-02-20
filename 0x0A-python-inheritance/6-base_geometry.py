f3bd5a38e383864e7d36680bd590739879931bf7
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-20 03:26:45 +0200

    Improve geomtry Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Base module
+"""
+
+
+class BaseGeometry:
+    """
+    my empty class
+    """
+    def area(self):
+        """
+        area of geometry
+        """
+        raise Exception("area() is not implemented")
+    pass
