d956b39d0e09df03eecada16f7bf9d2651fc37d4
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-20 09:51:02 +0200

    Task 11 ok

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+my module
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Mad
+    """
+    def __init__(self, size):
+        """
+        initializer
+        """
+        self.__size = size
+        Rectangle.integer_validator(self, "size", size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        return self.__size * self.__size
