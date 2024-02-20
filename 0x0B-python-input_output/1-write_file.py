684cb8a60c29af11fca393fc734cf411983f52d5
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-20 21:15:25 +0200

    added documentation on my function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,8 @@ Write into file module
 
 
 def write_file(filename="", text=""):
+    """
+    function to write into a file and return the number of character
+    """
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
