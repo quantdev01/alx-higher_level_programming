c2abdf838101354bc1cd1a922530fe5b1c1069a1
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-20 20:09:17 +0200

    Added a folder

---
+++ 0x0A-python-inheritance/lab/0-main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+with open("myfile.txt", encoding="utf-8") as file:
+        print(file.read())
---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module read text file and print it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    function to read the file and print it out
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
