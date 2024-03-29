dab023f60ecc1ca8d9872f0f482ed0321e1690c2
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-03-29 11:57:13 +0200

    Made all sql command capital

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- will select the dbs
-SELECT score, name from second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
