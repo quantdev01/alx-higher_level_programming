6f9f3ae46ca6b93db426620744116079e3f5e754
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-03-30 15:43:17 +0200

    changedMax_to_max_temp

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- Temperature by max state
-SELECT state, MAX(value) FROM temperatures GROUP BY state ORDER BY state ASC;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
