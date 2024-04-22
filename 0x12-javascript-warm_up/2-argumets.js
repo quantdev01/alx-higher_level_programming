12735a33d694e869b2da0154784f1aeed8402f87
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-04-22 23:24:40 +0200

    Task 6

---
+++ 0x12-javascript-warm_up/2-argumets.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const { argv } = 'node:process';
+
+console.log(argv.length);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myarray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+
+while (i < myarray.length) {
+  console.log(myarray[i]);
+  i++;
+}
