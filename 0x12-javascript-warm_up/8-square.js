d4cebf806abcef7311461fd4a60d958c51eb0f16
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-04-23 20:46:16 +0200

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,35 +2,25 @@
 
 const myArgs = process.argv;
 
-if (myArgs.length === 2)
+if (myArgs.length === 2 || isNaN(myArgs[2]))
 {
 	console.log('Missing size');
 }
-else if (typeof(myArgs[2]) === String)
-	console.log('Not a number');
 else
 {
 	let i = 0;
 	let j = 0;
 	let num = Number(myArgs[2]);
 
-	if (num !== NaN)
+	while (i < num)
 	{
-		console.log('The type is ' + typeof(num));
-		console.log('The conteng is ' + num);
-		while (i < num)
+		j = 0;
+		while (j < num)
 		{
-			j = 0;
-			while (j < num)
-			{
-				process.stdout.write('X');
-				j++;
-			}
-			console.log('');
-			i++;
+			process.stdout.write('X');
+			j++;
 		}
+		console.log('');
+		i++;
 	}
-	else
-		console.log('Not a number from inside');
-
 }
