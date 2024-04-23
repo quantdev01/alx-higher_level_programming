7591afe983c8b6e0902e1bacc74e1945bd7fccd6
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-04-23 20:55:00 +0200

    8. Square updated

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,25 +2,20 @@
 
 const myArgs = process.argv;
 
-if (myArgs.length === 2 || isNaN(myArgs[2]))
-{
-	console.log('Missing size');
-}
-else
-{
-	let i = 0;
-	let j = 0;
-	let num = Number(myArgs[2]);
+if (myArgs.length === 2 || isNaN(myArgs[2])) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  let j = 0;
+  const num = Number(myArgs[2]);
 
-	while (i < num)
-	{
-		j = 0;
-		while (j < num)
-		{
-			process.stdout.write('X');
-			j++;
-		}
-		console.log('');
-		i++;
-	}
+  while (i < num) {
+    j = 0;
+    while (j < num) {
+      process.stdout.write('X');
+      j++;
+    }
+    console.log('');
+    i++;
+  }
 }
