603f06f71b4f4e578286aa2d101a926cd19d4664
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-04-25 14:34:33 +0200

    2-rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,37 +3,34 @@
 /* class creation */
 
 class Rectangle {
-	width;
-	height;
+  width;
+  height;
 
-	constructor (w, h) {
-		if (w <= 0 || h <= 0 || isNaN(h) || isNaN(w)) {
-			delete this.width;
-			delete this.height;
-		} else {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(h) || isNaN(w)) {
+      delete this.width;
+      delete this.height;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print()
-	{
-		let i = 0;
-		let j = 0;
+  print () {
+    let i = 0;
+    let j = 0;
 
-		while(i < this.height)
-		{
-			j = 0;
+    while (i < this.height) {
+      j = 0;
 
-			while (j < this.width)
-			{
-				process.stdout.write('X');
-				j++;
-			}
-			console.log('');
-			i++;
-		}
-	}
+      while (j < this.width) {
+        process.stdout.write('X');
+        j++;
+      }
+      console.log('');
+      i++;
+    }
+  }
 }
 
 module.exports = Rectangle;
