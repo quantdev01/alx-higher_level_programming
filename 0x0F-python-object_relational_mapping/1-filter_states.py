034c76c61914b3d180d435dbe5c556daa1dda172
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-05-12 16:56:47 +0200

    Works now

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+args = sys.argv
+
+ln = len(args)
+
+if ln < 4:
+    print("Insufusant arguments, need 3 args")
+
+db = MySQLdb.connect(
+        host="localhost",
+        user=args[1],
+        passwd=args[2],
+        db=args[3],
+        port=3306
+        )
+
+cursor = db.cursor()
+try:
+    cursor.execute("SELECT * from states WHERE name LIKE 'N%'")
+    rows = cursor.fetchall()
+except e:
+    print("This happend {}".format(e))
+
+for row in rows:
+    print(row)
