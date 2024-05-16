6aee05342c26c4eaf7e27606ae189fea07d88e7a
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-05-16 21:35:28 +0200

    Good order priting now

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,30 +6,30 @@ const fs = require('fs');
 
 // Reading file content
 
-function readFile (filePath) {
-  try {
-    const data = fs.readFileSync(filePath, 'utf8');
-    return data;
-  } catch (err) {
-    console.error(`Error occured while reading the file \n ${err}`);
-  }
+function readFile(filePath){
+	try{
+		const data = fs.readFileSync(filePath, 'utf8');
+		return data;
+	} catch (err){
+		console.error(`Error occured while reading the file \n ${err}`);
+	}
 }
 
-const file1Content = readFile(args[2]);
-const file2Content = readFile(args[3]);
+let file1Content = readFile(args[2]);
+let file2Content = readFile(args[3]);
 
 // function to write in a given file
 
-function writeFile (filePath, content) {
-  fs.appendFile(filePath, content, 'utf8', (err) => {
-    if (err) {
-      console.error(`Error occured writing: ${err}`);
-    }
-  });
+function writeFile(filePath, content){
+	fs.appendFile(filePath, content, 'utf8', (err) =>{
+		if (err){
+			console.error(`Error occured writing: ${err}`);
+		}
+	});
 }
 
 const contentFile1 = file1Content;
 const contentFile2 = file2Content;
 
-writeFile(args[4], contentFile2);
 writeFile(args[4], contentFile1);
+writeFile(args[4], contentFile2);
