b17084aded0381d0c311c66f68daf48db08e66d5
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-06-14 16:58:58 +0200

    Added 7-model_state

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python3
 """ Documenting my python3 """
-from model_state import Base, State
 
-session = 
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+args = sys.argv
+
+ln = len(args)
+
+if ln <= 3:
+    print("Insufusant arguments, need 3 args")
+
+username = args[0]
+password = args[1]
+db_name = args[2]
+host = 'localhost'
+port = '3306'
+
+database_url = f"mysql+pymysql://{username}:{password}@{host}/{db_name}"
+engine = create_engine(database_url)
+
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+states = session.query(State).order_by(State.id).all()
+
+for state in states:
+    print(F"{state.id}: {state.name}")
