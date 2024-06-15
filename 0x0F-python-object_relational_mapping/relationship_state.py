a43e0c636ad1037a2e0724c54dd0998cf2ced6f0
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-06-15 16:20:41 +0200

    fixing pycodestyle on relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -14,4 +14,5 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
+    cities = relationship("City", back_populates="state", cascade="\
+all, delete-orphan")
