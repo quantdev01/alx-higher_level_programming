2f8a4f06e4958a377b8850afa780ffe49ce7d89a
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-06-15 15:25:34 +0200

    done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,13 +9,14 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Get MySQL username, password, and database name from the command line arguments
+    #  the command line arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
     # Create an engine to the specified database
-    engine = create_engine(f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}')
+    engine = create_engine(f'mysql+mysqldb://{mysql_username}:\
+{mysql_password}@localhost:3306/{database_name}')
 
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
